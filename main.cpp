fd0adfa708763da0a8a85b131bb798d9a1c98120
lyx <1320648097@qq.com>
2019-12-12 14:54:13 +0800

    项目的开始

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    /**/
+//    pid_t pid=fork();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
