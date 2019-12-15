700ca292f9e03feaf51c1a87e7d3a099bec596f2
lyx <1320648097@qq.com>
2019-12-15 16:14:20 +0800

    完成双击列表播放歌曲

--- mainwidgit.h
+++ mainwidgit.h
@@ -30,13 +30,16 @@ public:
     void closeEvent(QCloseEvent *e);
 
     /*普通函数*/
-    void music_list();
+    void music_list();//将文件中的所有歌曲存入列表中
+    void play_music(int row);//拼接路径、名字、文件类型，然后输入到终端播放
 
-    pid_t kill_pid;//成员变量传参，将进程号传过来控制子进程
+    /*公有成员变量*/
+    pid_t kill_pid;//成员变量传参，将进程号传过来控制子进程（因为需要类外使用，所以定义为公有的）
 
 public slots:
-    void label_func(int mod);
-    void close_list();
+    void label_func(int mod);//控制播放按钮
+    void close_list();//能够自关列表
+    void play_list_music();//播放列表中的歌曲
 
 signals:
     void kill_mplayer();
@@ -55,16 +58,18 @@ private:
     Mlabel *label_pause;//设置（播放和暂停）按键
     Mlabel *label_back;//设置切换上一首歌按钮
     Mlabel *label_front;//设置切换下一首歌按钮
+    int play_pause;//控制暂停播放按钮的图片加载
+    int fd;//控制打开管道，进行命令的输入或读取
 
     QToolBox *toolbox_list;//设置
     QList<QAbstractButton *>toolbox_button;//这个是用来存放toolbox_list下级的按钮指针
-//    int n;//用来控制(toolbox_list)按钮的自开自关
     QListWidget *list_bendi;
     QListWidget *list_moren;
     QListWidget *list_last;//设为最后一个作为自关的显示
 
-    int music_num;
-    QStringList music_all_name;
+    int music_num;//获得总共歌曲的数量，方便输入列表
+    int music_currentrow;//当前歌曲在列表中的位置，如果重新读取本地文件夹一切重置
+    QStringList music_all_name;//将歌曲的名字存入链表中
 };
 
 #endif // MAINWIDGIT_H
